c9124a40a410179bab346d8060fd014b1ac6da2e
Patey <brandenpatey@hotmail.com>
2015-06-07 04:02:10 -0230

    Working on registering player info

--- src/main/java/com/gmail/Patey07/DFSCore.java
+++ src/main/java/com/gmail/Patey07/DFSCore.java
@@ -2,6 +2,9 @@ package com.gmail.Patey07;
 
 	import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
@@ -10,13 +13,9 @@ import ninja.leaping.configurate.loader.ConfigurationLoader;
 
 import org.slf4j.Logger;
 import org.spongepowered.api.event.Subscribe;
-import org.spongepowered.api.event.state.PreInitializationEvent;
-import org.spongepowered.api.event.state.ServerStartedEvent;
 import org.spongepowered.api.event.state.ServerStartingEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.service.config.ConfigDir;
-import org.spongepowered.api.service.config.DefaultConfig;
-
 import com.google.inject.Inject;
 
 	@Plugin(id = "DFSCore", name = "DFS-Core", version = "0.1.0")
@@ -33,18 +32,19 @@ import com.google.inject.Inject;
 		    return logger;
 		}
 		
-		@Inject @ConfigDir(sharedRoot = false) File configDir;
+		@Inject @ConfigDir(sharedRoot = false) private File configDir;
 		
 		File coreFile = new File(this.configDir,"main.conf");
 		ConfigurationLoader<CommentedConfigurationNode> mainConfig = HoconConfigurationLoader.builder().setFile(coreFile).build();
 		File userFile = new File(this.configDir,"users.conf");
 		ConfigurationLoader<CommentedConfigurationNode> userConfig = HoconConfigurationLoader.builder().setFile(userFile).build();
 		
-		ConfigurationNode config;
-		ConfigurationNode config2;
+
 		
 		@Subscribe
 	    public void onServerStart(ServerStartingEvent event) {
+			ConfigurationNode config = null;
+			ConfigurationNode config2 = null;
 			try {
 				if (!coreFile.exists()) {
 					coreFile.createNewFile();
@@ -57,10 +57,12 @@ import com.google.inject.Inject;
 				if (!userFile.exists()) {
 					userFile.createNewFile();
 					config2 = userConfig.load();
-					
-					config2.getNode("uuid_here","Name").setValue("Patey");
-					config2.getNode("uuid_here","Race").setValue("human");
-					config2.getNode("uuid_here","Channel").setValue("global");
+					String[][] playerList = { {"UUID1","Patey","human","global"},{"UUID2","Patey2","human2","global2"}};
+					for (int i=0; i<playerList.length; i++){
+						List<String> playerLista = new ArrayList<String>(Arrays.asList(playerList[i])); 
+						config2.getNode("Players",playerList[i][0]).setValue(playerLista);
+					}
+					config2.getNode("test").setValue("test");
 					userConfig.save(config2);
 					getLogger().info("Default user config created at" + userFile.getAbsolutePath().substring(0,userFile.getAbsolutePath().lastIndexOf(File.separator)));
 				}
@@ -69,7 +71,7 @@ import com.google.inject.Inject;
 			} catch (IOException exception) {
 			    getLogger().error("Configurations could not be loaded or created!");
 			}
-			
+			event.getGame().getEventManager().register(this, new PlayerEvents(config2,userConfig));
 			getLogger().info("DwarfFortressSuite Core initialized");
 	    }
 	}
--- src/main/java/com/gmail/Patey07/PlayerEvents.java
+++ src/main/java/com/gmail/Patey07/PlayerEvents.java
@@ -1,17 +1,97 @@
 package com.gmail.Patey07;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+
+import org.slf4j.Logger;
 import org.spongepowered.api.event.Subscribe;
-import org.spongepowered.api.event.entity.player.PlayerBreakBlockEvent;
 import org.spongepowered.api.event.entity.player.PlayerJoinEvent;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.Texts;
+import org.spongepowered.api.text.format.TextColors;
+
+import com.google.common.base.Function;
 
 public class PlayerEvents {
+	String[][] playerList = null;
+	ConfigurationNode eventNode;
+	ConfigurationLoader<CommentedConfigurationNode> eventConfig;
+	
+	public Logger getLogger() {
+	    return DFSCore.getLogger();
+	}
+	
+	public PlayerEvents(ConfigurationNode rootNode,ConfigurationLoader<CommentedConfigurationNode> userConfig){
+		Function<Object, String> stringTransformer = new Function<Object,String>() {
+		    public String apply(Object input) {
+		        if (input instanceof String) {
+		            return (String) input;
+		        } else {
+		            return null;
+		        }
+		    }
+		};
+		Object[] tempUUIDS = rootNode.getNode("Players").getChildrenMap().keySet().toArray();
+		playerList = new String[tempUUIDS.length][4];
+		String[] playerUUIDS = new String[tempUUIDS.length];
+		for (int k=0; k<tempUUIDS.length; k++){
+			playerUUIDS[k] = tempUUIDS[k].toString();
+		}
+		getLogger().info(Integer.toString(playerUUIDS.length));
+		for (int i=0; i < playerUUIDS.length; i++){
+			String player = playerUUIDS[i];
+			List<String> tempList = rootNode.getNode("Players",player).getList(stringTransformer);
+			String[] tempArr = new String[tempList.size()];
+			tempArr = tempList.toArray(tempArr);
+			playerList[i] = tempArr;
+		}
+		eventNode = rootNode;
+		eventConfig = userConfig;
+	}
+	
+	public Text getMessage(String message) {
+	    return Texts.of(message).builder().color(TextColors.BLUE).build();
+	}
+	
 	@Subscribe
-	public void onBreak (PlayerJoinEvent jEvent) {
-		UUID player = jEvent.getUser().getUniqueId();
+	public void onJoin (PlayerJoinEvent jEvent) {
+		String player = jEvent.getUser().getUniqueId().toString();
+		String playerName = jEvent.getUser().getName();
+		
+		for (int i=0; i<playerList.length; i++){
+			if (playerList[i][0].equals(player) && playerList[i][1].equals(playerName)){
+				jEvent.getUser().sendMessage(getMessage("Welcome back "+playerList[i][1]+"!"));
+			}else if (playerName.equals(playerList[i][1]) && player != playerList[i][0]){
+				jEvent.getUser().kick(getMessage("That's somone else's name!"));
+			}else if (playerList[i][0].equals(player) && playerList[i][1] != playerName){
+				jEvent.getUser().kick(getMessage("Sorry,"+playerName+"no multiple names untill sponge updates"));
+			}else if (i == playerList.length){
+				String[] tempArr = new String[playerList.length+1];
+				String[][] temp2D = new String[tempArr.length][4];
+				for (int j=0; j<tempArr.length; j++){
+					if (j==tempArr.length){
+						String[] tempPlayer = {player, playerName ,"spirit","global"};
+						temp2D[j] = tempPlayer;
+						playerList[j] = temp2D[j];
+						eventNode.getNode("Players",playerList[i][0]).setValue(playerList[j]);
+						try {
+							eventConfig.save(eventNode);
+						} catch (IOException e) {
+							getLogger().error("user config could not be edited for "+playerName);
+						}
+					}else{
+						temp2D[j] = playerList[j];
+					}
+				}
+				
+				
+			}
+		}
 		
 	}
 }
